00600f53cefac53674aba211dd20685f03567de2
noahdominic <noahdmnc@gmail.com>
2023-12-09 01:57:23 +0800

    Opt Day 8 python

--- 08-haunted-wasteland/main.py
+++ 08-haunted-wasteland/main.py
@@ -1,28 +1,22 @@
 import re, time
 
-LEFT=0
-RIGHT=1
-
 def gcd(a, b):
     while b:
         a, b  =  b, a % b
     return a
 
-def walk_through_map(lr_seq, map_arr, start):
+def walk_through_map(lr_seq, map_dict, start):
     current_loc = start
     lr_seq_len = len(lr_seq)
-    i = lr_seq_i = 0
-    
+    i = 0
     # In my input file, this condition works fine for Part 1.  For some reason
     # 'AAA' does not go to any location ending with 'Z' other than 'ZZZ'.  This
     # may not be the same for you.  If that's so, just duplicate this function,
     # name it something else, and then change the condition to
     #    `current_loc != 'ZZZ'`
     while current_loc[2] != 'Z': 
-        current_loc = map_arr[current_loc][LEFT if lr_seq[lr_seq_i] == 'L' else RIGHT]
+        current_loc = map_dict[current_loc][lr_seq[i % lr_seq_len] == 'R']
         i += 1
-        lr_seq_i = (lr_seq_i + 1) % lr_seq_len
-
     return i
 
 def main():
@@ -34,21 +28,25 @@ def main():
     lr_seq, maps = lns[0], lns[2:]
     maps = [list(filter(None, re.split(r'\s=\s\(|,\s|\)', x))) for x in maps]
     map_dict = {map[0]: (map[1], map[2]) for map in maps}
-    a = time.time()
+    
     for i, j in ((i, j) for i in range(26) for j in range(26)):
         start_loc = alphabet[i] + alphabet[j] + 'A'
         if start_loc in map_dict:
             num_steps = walk_through_map(lr_seq, map_dict, start_loc)
             answer = answer * num_steps // gcd(answer, num_steps)
-    b = time.time()
-    c = time.time()
-    print("Part 1: {0}".format(walk_through_map(lr_seq, map_dict, 'AAA')))
-    d = time.time()
 
+    print("Part 1: {0}".format(walk_through_map(lr_seq, map_dict, 'AAA')))
     print("Part 2: {0}".format(answer))
-    print(d - c)
-    print(b - a)
-    return
 
 if __name__ == "__main__":
     main()
+
+
+with open("input.txt") as f: 
+    lines_str = f.readlines()
+    lines_no_space = [line.strip() for line in lines_str]
+
+    leftright_seq = lines_no_space[0]
+    maps_data_str = lines_no_space[2:]
+    maps_data_parsed = [list(filter(None, re.split(r'\s=\s\(|,\s|\)', x))) for x in maps_data_str]
+    maps_dict = {map[0]: (map[1], map[2]) for map in maps_data_parsed}
