d4371271fa932bfc553bb55d0ed3f6382ba62b42
noahdominic <noahdmnc@gmail.com>
2023-12-10 00:32:23 +0800

    rm main.py; mv main-min.py main.py

--- 09-mirage-maintenance/main.py
+++ 09-mirage-maintenance/main.py
@@ -1,77 +1,12 @@
-import numpy as np
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-
-def get_prev(sequence):
-    diffs = sequence 
-    prev = diffs[0]
-    is_subtr = True
-    while not all(x==0 for x in diffs):
-        diffs = [next - curr for curr, next in zip(diffs[:-1], diffs[1:])]
-        prev = prev - diffs[0] if is_subtr else prev + diffs[0]
-        is_subtr = not is_subtr
-   
-    return prev
-
 def get_next(sequence):
-    diffs = sequence 
-    degree = 1
-    next = diffs[-1]
-    while not all(x==0 for x in diffs):
-        diffs = [next - curr for curr, next in zip(diffs[:-1], diffs[1:])]
-        next += diffs[-1]
-        degree += 1
-   
-    return next
-
-def get_deg(sequence):
-    diffs = sequence 
-    degree = 1
-    while not any(x==0 for x in diffs):
-        diffs = [next - curr for curr, next in zip(diffs[:-1], diffs[1:])]
-        degree += 1
-   
-    return degree
-
-def predict_next_number(sequence):
-    # Creating features and target
-    X = np.array(range(1, len(sequence) + 1)).reshape(-1, 1)
-    y = np.array(sequence)
-
-    # Creating polynomial features
-    poly = PolynomialFeatures(degree=get_deg(sequence))
-    X_poly = poly.fit_transform(X)
-
-    # Fitting the polynomial regression model
-    model = LinearRegression()
-    model.fit(X_poly, y)
-
-    # Predicting the next number
-    next_number = model.predict(poly.transform([[len(sequence) + 1]]))
-    predicted_number = int(round(next_number[0]))
-
-    # Calculating RMSE
-    y_pred = model.predict(X_poly)
-    rmse = np.sqrt(mean_squared_error(y, y_pred))
-
-    return predicted_number, rmse
+    sum = 0
+    while not all(x == 0 for x in sequence):
+        sum += sequence[-1]
+        sequence = [sequence[i + 1] - sequence[i] for i in range(len(sequence) - 1)]
+    return sum
 
 with open("input.txt") as f:
-    lines = f.readlines()
-
-lines = [[int(num) for num in ln.split()] for ln in lines]
-
-total1 = 0
-total2 = 0
-
-for sequence in lines:
-    actual_next_number = get_next(sequence)
-    actual_prev_number = get_prev(sequence)
-    total1 += actual_next_number
-    total2 += actual_prev_number
-    # print("Next: {0}\tPrev:{1}".format(actual_next_number, actual_prev_number))
-    print("Next: {0}\tPrev: {1}".format(actual_next_number, actual_prev_number))
-
-print(total1)
-print(total2)
+    lines = [[int(num) for num in ln.split()] for ln in f.readlines()]
+    
+print(sum(get_next(sequence) for sequence in lines))
+print(sum(get_next(sequence[::-1]) for sequence in lines))
