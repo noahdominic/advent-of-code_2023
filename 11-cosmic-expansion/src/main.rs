b300c25e2ed15655eb5427cc5c0770d935d57cbf
noahdominic <noahdmnc@gmail.com>
2023-12-12 21:24:46 +0800

    Day 11.1, 11.2 More efficient sol

--- 11-cosmic-expansion/src/main.rs
+++ 11-cosmic-expansion/src/main.rs
@@ -10,39 +10,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .filter_map(|line| line.ok().map(|l| l.trim().chars().collect()))
         .collect::<Vec<Vec<char>>>();
 
-    let (galaxy_coords, (empty_rows, empty_cols)) = get_empty_rows_and_cols(&grid);
+    let galaxy_coords = get_expanded_galaxy_coords(&grid, 1000000 - 1);
 
     let mut total_distance = 0;
 
     for i in 0..galaxy_coords.len() {
         for j in i..galaxy_coords.len() {
-            let distance = get_expanded_manhattan_distance(
-                galaxy_coords[i],
-                galaxy_coords[j],
-                &empty_rows,
-                &empty_cols,
-                1000000 - 1,
-            );
-
-            // println!(
-            //     "{:?} to {:?} is distance {:?}",
-            //     galaxy_coords[i], galaxy_coords[j], distance
-            // );
-
-            total_distance += distance;
+            total_distance += get_manhattan_distance(galaxy_coords[i], galaxy_coords[j]);
         }
-        // println!();
     }
 
     println!("{:?}", total_distance);
 
-    // println!("{:?}", galaxy_coords);
     Ok(())
 }
 
-fn get_empty_rows_and_cols(
+fn get_expanded_galaxy_coords(
     grid: &Vec<Vec<char>>,
-) -> (Vec<(usize, usize)>, (HashSet<usize>, HashSet<usize>)) {
+    expansion_factor: usize,
+) -> Vec<(usize, usize)> {
     let mut empty_rows: HashSet<usize> = (0..grid.len()).collect();
     let mut empty_cols: HashSet<usize> = (0..grid[0].len()).collect();
     let mut galaxies: Vec<(usize, usize)> = Vec::new();
@@ -57,7 +43,24 @@ fn get_empty_rows_and_cols(
         }
     }
 
-    return (galaxies, (empty_rows, empty_cols));
+    // expand galaxy coords here
+    for i in 0..galaxies.len() {
+        let expansion_col = empty_cols
+            .iter()
+            .filter(|&&col| col < galaxies[i].1)
+            .collect::<Vec<&usize>>()
+            .len()
+            * expansion_factor;
+        let expansion_row = empty_rows
+            .iter()
+            .filter(|&&row| row < galaxies[i].0)
+            .collect::<Vec<&usize>>()
+            .len()
+            * expansion_factor;
+        galaxies[i] = (galaxies[i].0 + expansion_row, galaxies[i].1 + expansion_col)
+    }
+
+    return galaxies;
 }
 
 fn get_manhattan_distance(p1: (usize, usize), p2: (usize, usize)) -> usize {
@@ -66,47 +69,6 @@ fn get_manhattan_distance(p1: (usize, usize), p2: (usize, usize)) -> usize {
 
     let dcol = col2.abs_diff(col1);
     let drow = row2.abs_diff(row1);
-
-    dcol + drow
-}
-
-fn get_expanded_manhattan_distance(
-    p1: (usize, usize),
-    p2: (usize, usize),
-    empty_rows: &HashSet<usize>,
-    empty_cols: &HashSet<usize>,
-    expansion_factor: usize,
-) -> usize {
-    let (row1, col1) = p1;
-    let (row2, col2) = p2;
-
-    let (col1, col2) = if col1 < col2 {
-        (col1, col2)
-    } else {
-        (col2, col1)
-    };
-
-    let (row1, row2) = if row1 < row2 {
-        (row1, row2)
-    } else {
-        (row2, row1)
-    };
-
-    let expansion_col = empty_cols
-        .iter()
-        .filter(|&&col| col > col1 && col < col2)
-        .collect::<Vec<&usize>>()
-        .len()
-        * expansion_factor;
-    let expansion_row = empty_rows
-        .iter()
-        .filter(|&&row| row > row1 && row < row2)
-        .collect::<Vec<&usize>>()
-        .len()
-        * expansion_factor;
-
-    let dcol = col2.abs_diff(col1) + expansion_col;
-    let drow = row2.abs_diff(row1) + expansion_row;
 
     dcol + drow
 }
